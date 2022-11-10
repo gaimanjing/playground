7a16d98be776d2b6605c6a40859f5d3f35070877
gaimanjing <gaimanjing@gmail.com>
2022-11-10 14:15:43 +0800

    unique_ptr_tests

--- playground/playgroundTests/cpp/unique_ptr_tests.mm
+++ playground/playgroundTests/cpp/unique_ptr_tests.mm
@@ -9,6 +9,8 @@
 
 #include "m9.hpp"
 
+#include <fstream>
+
 @interface unique_ptr_tests : XCTestCase
 
 @end
@@ -31,6 +33,48 @@
         std::unique_ptr<D> q = pass_through(std::move(p));
         
         assert(!p);
+    }
+    
+    std::cout << "\n" "2) Runtime polymorphism demo\n";
+    {
+        std::unique_ptr<B> p = std::make_unique<D>();
+        
+        p->bar();
+    }
+    
+    std::cout << "\n""3) Custom deleter demo\n";
+    std::ofstream("demo.txt") << 'x';
+    {
+        using unique_file_t = std::unique_ptr<std::FILE, decltype(&close_file)>;
+        unique_file_t fp(std::fopen("demo.txt", "r"), &close_file);
+        if (fp) {
+            std::cout << char(std::fgetc(fp.get())) << '\n';
+        }
+    }
+    
+    std::cout << "\n" "4) Custom lambda-expression deleter and exception safety demo\n";
+    try {
+        std::unique_ptr<D, void(*)(D*)> p(new D, [](D* ptr){
+            std::cout << "destroying from a custom deleter...\n";
+            delete ptr;
+        });
+        throw std::runtime_error("");
+    } catch (const std::exception&) {
+        std::cout << "Caught exception\n";
+    }
+    
+    std::cout << "\n" "5) Array form of unique_prt demo\n";
+    {
+        std::unique_ptr<D[]> p(new D[3]);
+    }
+        
+    std::cout << "\n" "6) Linked list demo\n";
+    {
+        List wall;
+        for (int beer = 0; beer != 1'000'000; ++beer)
+            wall.push(beer);
+        
+        std::cout << "1'000'000 bottles of beer on the wall...\n";
     }
 }
 
